cfbe119194a937cd1ea75fafc096863522a5a796
Paulo J Santos <pjsantospaulo@gmail.com>
2016-03-07 22:41:11 -0400

    Primeiro Commit

---
+++ Negocio/Model/Paciente.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Model
+{
+    public class Paciente
+    {
+        private int pacienteId;
+        public int PacienteId
+        {
+            get { return pacienteId; }
+            set { pacienteId = value; }
+        }
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value; }
+        }
+        private DateTime dtNascimento;
+        public DateTime DtNascimento
+        {
+            get { return dtNascimento; }
+            set { dtNascimento = value; }
+        }
+        private string tipoConveniado;
+        public string TipoConveniado
+        {
+            get { return tipoConveniado; }
+            set { tipoConveniado = value; }
+        }
+        private string planoSaude;
+        public string PlanoSaude
+        {
+            get { return planoSaude; }
+            set { planoSaude = value; }
+        }
+    }
+}
---
+++ Negocio/Model/PlanoSaude.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio.Model
+{
+    public class PlanoSaude
+    {
+        private int planoDeSaudeId;
+        public int PlanoSaudeId
+        {
+            get { return planoDeSaudeId; }
+            set { planoDeSaudeId = value; }
+        }
+        private string descricao;
+        public string Descricao
+        {
+            get { return descricao; }
+            set { descricao = value; }
+        }
+
+    }
+}
